dcc3ad58f2115f9ad553a59f2d3f90e61826b6f6
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-06-13 22:07:23 +0200

    19.4 with problem

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,8 +5,43 @@ import PropTypes from 'prop-types';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
 
-const OrderForm = ({tripCost, options, setOrderOption}) => (
+const sendOrder = (options, tripCost, tripDetails) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    ...tripDetails,
+
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
+
+
+const OrderForm = ({tripCost, options, setOrderOption, tripDetails}) => (
   <Row>
     {pricing.map((option) => 
       <Col md={4} key={option.id}>
@@ -17,6 +52,7 @@ const OrderForm = ({tripCost, options, setOrderOption}) => (
       <OrderSummary cost={tripCost} options={options}/>
 
     </Col>
+    <Button onClick={() => sendOrder(options, tripCost, tripDetails)}>Order now!</Button>
   </Row>
 );
 
@@ -24,6 +60,7 @@ OrderForm.propTypes = {
   tripCost: PropTypes.any,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  tripDetails: PropTypes.any,
 };
 
 export default OrderForm;
