032a7a71eee757421cf26fb2ea71d48852110a23
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-08-01 18:17:42 +0200

    TTD 7 test - 20.2

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -4,16 +4,30 @@ import styles from './HappyHourAd.scss';
 
 
 class HappyHourAd extends React.Component {
+  constructor(){
+    super();
+      
+    /* run this.forceUpdate() every second */
+    setInterval(() => {
+      this.forceUpdate();
+    }, 1000);
+      
+  }
+
 
   getCountdownTime(){
     const currentTime = new Date();
     const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
       
-    if(currentTime.getUTCHours() >= 12){
-      nextNoon.setUTCDate(currentTime.getUTCDate()+1);
+    if(currentTime.getUTCHours() === 12) {
+      return 'It is your time! Take advantage of Happy Hour! All offers 20% off!';
+    } else {
+  
+      if(currentTime.getUTCHours() >= 12){
+        nextNoon.setUTCDate(currentTime.getUTCDate()+1);
+      }
+      return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
     }
-      
-    return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
   }
 
   render(){
